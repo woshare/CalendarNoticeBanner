3d676c30d6656a1f39b50506e96a2ac597801c5b
woshare <liu.jun_T02544@hellogroup.com>
2026-06-13 19:18:33 +0800

    feat: wire up all modules in AppDelegate

--- CalendarBanner/CalendarBannerApp.swift
+++ CalendarBanner/CalendarBannerApp.swift
@@ -6,13 +6,44 @@ struct CalendarBannerApp: App {
 
     var body: some Scene {
         Settings {
-            EmptyView()
+            SettingsView(preferences: AppDelegate.shared.preferences)
         }
     }
 }
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    static var shared: AppDelegate!
+
+    let preferences = PreferencesStore()
+    private var bannerController: BannerWindowController!
+    private var calendarMonitor: CalendarMonitor!
+    private var menuBarController: MenuBarController!
+
+    override init() {
+        super.init()
+        AppDelegate.shared = self
+    }
+
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // 后续 Task 9 在此初始化 MenuBarController
+        bannerController = BannerWindowController(preferences: preferences)
+        calendarMonitor = CalendarMonitor(preferences: preferences)
+        menuBarController = MenuBarController(
+            preferences: preferences,
+            bannerController: bannerController
+        )
+
+        calendarMonitor.onTrigger = { [weak self] event, minutesBefore in
+            self?.bannerController.show(event: event, minutesBefore: minutesBefore)
+        }
+
+        calendarMonitor.onPermissionDenied = { [weak self] in
+            self?.menuBarController.showPermissionWarning()
+        }
+
+        calendarMonitor.start()
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        calendarMonitor.stop()
     }
 }
